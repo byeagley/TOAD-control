b0139df758a3e1b31d9261d4dfa9e2d663c13d77
byeagley <byeagley@github.iu.edu>
2021-03-08 14:55:38 -0500

    sliders now have corresponding outputs

--- control-GUI.py
+++ control-GUI.py
@@ -37,10 +37,24 @@ alt2Color = GREY
 ackermannText = font.render('Ackermann steering', True, BLACK)
 tankText = font.render('Tank mode', True, BLACK)
 
-# Speed Slider
-speed = 0
+drivingMode = "Ackermann"
 
+# Speed Sliders
+leftspeed = 0
+rightspeed = 0
+rightsliderBackground = pygame.Rect(200, 400, 20, 350)
+leftsliderBackground = pygame.Rect(100, 400, 20, 350)
 
+rightsliderButton = pygame.Rect(202, 720, 16, 30)
+leftsliderButton = pygame.Rect(102, 720, 16, 30)
+
+pygame.draw.rect(guiDisplay, GREY, rightsliderBackground)
+pygame.draw.rect(guiDisplay, GREY, leftsliderBackground)
+pygame.draw.rect(guiDisplay, WHITE, rightsliderButton)
+pygame.draw.rect(guiDisplay, WHITE, leftsliderButton)
+
+rightsliderDrag = False
+leftsliderDrag = False
 
 # Steering Angle Slider
 steering_angle = 0
@@ -71,12 +85,12 @@ while running:
             if event.key == pygame.K_RIGHT:
                 if steering_angle < 30:
                     steering_angle += 5
-            if event.key == pygame.K_UP:
-                if speed < 100:
-                    speed += 10
-            if event.key == pygame.K_DOWN:
-                if speed > 0:
-                    speed -= 10
+            #if event.key == pygame.K_UP:
+                #if speed < 100:
+                    #speed += 10
+            #if event.key == pygame.K_DOWN:
+                #if speed > 0:
+                    #speed -= 10
             
         pygame.draw.rect(guiDisplay, alt1Color, ackermannButton)
         pygame.draw.rect(guiDisplay, alt2Color, tankButton)
@@ -87,13 +101,44 @@ while running:
         angleDisplay = font.render('Steering angle: ' + str(steering_angle), True, WHITE)
         guiDisplay.blit(angleDisplay, (400, 600))
 
-        speedDisplay = font.render('Speed: ' + str(speed), True, WHITE)
-        guiDisplay.blit(speedDisplay, (400, 630))
+        #speedDisplay = font.render('Speed: ' + str(speed), True, WHITE)
+        #guiDisplay.blit(speedDisplay, (400, 630))
 
         pos = pygame.mouse.get_pos()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
+            if rightsliderButton.collidepoint(event.pos):
+                    rightsliderDrag = True
+                    mouse_x, mouse_y = event.pos
+                    offset_x = 0
+                    offset_y = rightsliderButton.y - mouse_y
+            elif leftsliderButton.collidepoint(event.pos):
+                    leftsliderDrag = True
+                    mouse_x, mouse_y = event.pos
+                    offset_x = 0
+                    offset_y = leftsliderButton.y - mouse_y
             mouseClicked = True
+        
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:   
+                rightsliderDrag = False         
+                leftsliderDrag = False
+
+        elif event.type == pygame.MOUSEMOTION:
+            if rightsliderDrag:
+                mouse_x, mouse_y = event.pos
+                rightsliderButton.y = mouse_y + offset_y
+                if rightsliderButton.y > 720:
+                    rightsliderButton.y = 720
+                elif rightsliderButton.y < 400:
+                    rightsliderButton.y = 400
+            elif leftsliderDrag:
+                mouse_x, mouse_y = event.pos
+                leftsliderButton.y = mouse_y + offset_y
+                if leftsliderButton.y > 720:
+                    leftsliderButton.y = 720
+                elif leftsliderButton.y < 400:
+                    leftsliderButton.y = 400
 
         if power_button.collidepoint(pos) and mouseClicked:
             temp = currentColor
@@ -114,6 +159,18 @@ while running:
                 alt1Color = GREY
                 alt2Color = WHITE
 
+        pygame.draw.rect(guiDisplay, GREY, rightsliderBackground)
+        pygame.draw.rect(guiDisplay, GREY, leftsliderBackground)
+        pygame.draw.rect(guiDisplay, WHITE, rightsliderButton)
+        pygame.draw.rect(guiDisplay, WHITE, leftsliderButton)
+
+        leftspeed = round( (leftsliderButton.y - 720) / -3.2 )
+        rightspeed = round( (rightsliderButton.y - 720) / -3.2 )
+
+        leftspeedDisplay = font.render('Left motor speed: ' + str(leftspeed), True, WHITE)
+        guiDisplay.blit(leftspeedDisplay, (400, 630))
+        rightspeedDisplay = font.render('Right motor speed: ' + str(rightspeed), True, WHITE)
+        guiDisplay.blit(rightspeedDisplay, (400, 660))
 
 
         pygame.display.update()
