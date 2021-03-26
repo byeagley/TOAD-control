973360ad8a924e9c6b3590ad9c47607e434f77e3
byeagley <byeagley@github.iu.edu>
2021-03-26 18:17:49 -0400

    GUI progress

--- demo.py
+++ demo.py
@@ -1,11 +1,24 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication,
                              QHBoxLayout, QVBoxLayout, QGridLayout)
  
 from PyQt5.QtCore import Qt
 import sys
+import json
 
- 
+parameters = {
+        'power': 'off', 
+        'mode': 'Ackermann',
+        'forward speed': 0,
+        'steering angle': 0,
+        'turn speed': 50,
+        'turning': False
+        }
+
+with open('output.txt', 'w') as json_file:
+    json.dump(parameters, json_file, indent = len(parameters))
+
+
 class PyQtLayout(QWidget):
  
     def __init__(self):
@@ -19,13 +32,16 @@ class PyQtLayout(QWidget):
             if combo.currentText() == 'Ackermann':
                 dial.setVisible(True)
                 slider_label.setText("Forward speed")
-                #slider.setValue(0)
+                slider.setValue(0)
+                dial.setValue(0)
                 turnButton.setVisible(False)
+                angle_label.setVisible(True)
             if combo.currentText() == 'Turn in Place':
                 dial.setVisible(False)
                 slider_label.setText("Turn speed")
-                #slider.setValue(50)
+                slider.setValue(50)
                 turnButton.setVisible(True)
+                angle_label.setVisible(False)
         
         def changeColor():
             if button.isChecked():
@@ -36,14 +52,41 @@ class PyQtLayout(QWidget):
         def speedDisplay(speed):
             speed_label.setText(str(speed))
         
+        def angleDisplay(angle):
+            angle_label.setText(str(angle))
+
+        def json_output():
+            parameters['mode'] = combo.currentText()
+
+            if combo.currentText() == 'Ackermann':
+                parameters['forward speed'] = slider.value()
+                parameters['steering angle'] = dial.value()
+            elif combo.currentText() == 'Turn in Place':
+                parameters['turn speed'] = slider.value()
+
+            if turnButton.isDown():
+                parameters['turning'] = True
+            else:
+                parameters['turning'] = False
+
+            if button.isChecked():
+                parameters['power'] = 'on'
+            else:
+                parameters['power'] = 'off'
+            with open('output.txt', 'w') as json_file:
+                json.dump(parameters, json_file, indent = len(parameters))
+        
         # Turn in Place Button
-        turnButton = QPushButton('Turn')
+        turnButton = QPushButton('Hold to Turn')
         turnButton.setVisible(False)
+        turnButton.pressed.connect(json_output)
+        turnButton.released.connect(json_output)
 
         # Power Button
         button = QPushButton('Power')
         button.setCheckable(True)
         button.clicked.connect(changeColor)
+        button.clicked.connect(json_output)
         button.setMaximumWidth(50)
         button.setStyleSheet("background-color : red") 
         
@@ -51,6 +94,7 @@ class PyQtLayout(QWidget):
         combo = QtWidgets.QComboBox(self)
         combo.addItems(["Ackermann", "Turn in Place"])
         combo.currentIndexChanged.connect(switchMode)
+        combo.currentIndexChanged.connect(json_output)
 
         # Angle Dial
         dial = QtWidgets.QDial(self)
@@ -59,6 +103,8 @@ class PyQtLayout(QWidget):
         dial.setNotchesVisible(True)
         dial.setNotchTarget(2)
         dial.move(270,300)
+        dial.valueChanged.connect(angleDisplay)
+        dial.valueChanged.connect(json_output)
         
         # Speed Slider
         slider = QtWidgets.QSlider(Qt.Vertical, self)
@@ -67,6 +113,7 @@ class PyQtLayout(QWidget):
         slider.setTickPosition(QtWidgets.QSlider.TicksBelow)
         slider.setTickInterval(10)
         slider.valueChanged.connect(speedDisplay)
+        slider.valueChanged.connect(json_output)
 
         # Slider label    
         slider_label = QtWidgets.QLabel(self)
@@ -76,10 +123,19 @@ class PyQtLayout(QWidget):
         # Speed label
         speed_label = QtWidgets.QLabel(self)
         speed_label.setText("0")
-        
+        speed_label.setStyleSheet('QLabel { background: #007AA5; border-radius: 3px;}')
+        speed_label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+        speed_label.setFont(QtGui.QFont("Arial",25))
+
+        # Angle Label
+        angle_label = QtWidgets.QLabel(self)
+        angle_label.setText("0")
+        angle_label.setFont(QtGui.QFont("Arial",25))
+        angle_label.setStyleSheet('QLabel { background: #007AA5; border-radius: 3px;}')
+        angle_label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
 
 
-        # Layout Management 
+        # Grid Layout Management 
         vbox = QGridLayout()
         vbox.addWidget(combo, 0, 2)
         vbox.addWidget(dial, 1, 2)
@@ -87,7 +143,9 @@ class PyQtLayout(QWidget):
         vbox.addWidget(slider_label, 0, 0)
         vbox.addWidget(speed_label, 2, 0)
         vbox.addWidget(button, 2, 2)
-        vbox.addWidget(turnButton, 1, 1)
+        vbox.addWidget(turnButton, 1, 1, 1, 2)
+        vbox.addWidget(angle_label, 1, 1)
+
  
         self.setLayout(vbox)
         self.setGeometry(300, 300, 350, 250)
