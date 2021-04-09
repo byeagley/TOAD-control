b5c2e812cc5c4cbdf610c8742e0159d1c208b3b0
byeagley <46436812+byeagley@users.noreply.github.com>
2021-04-09 19:59:03 -0400

    update

--- marker_detection.py
+++ marker_detection.py
@@ -2,4 +2,15 @@ import cv2 as cv
 import numpy as np
 import cv.aruco as aruco
 
+parameters =  cv.aruco.DetectorParameters_create()
+aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)
+
+cap = cv.VideoCapture(1) 
+
+def track(matrix_coeffs, distortion_coeffs):
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        corners, ids, rejected_img_points = aruco.detectMarkers(gray, aruco_dict, parameters=parameters, cameraMatrix=matrix_coeffs, distCoeff=distortion_coeffs)
 
